920d6094ec4ecf0b92185279ca4647e5ebedc6c1
Michel Corrêa <micheloscbc@hotmail.com>
2021-07-19 23:53:58 -0300

    Adicionando arquivo gitignore

--- src/test/java/com/algaworks/algafoodapi/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafoodapi/CadastroCozinhaIT.java
@@ -1,2 +1,49 @@
-package com.algaworks.algafoodapi;public class CadastroCozinhaIT {
+package com.algaworks.algafoodapi;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CadastroCozinhaIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void deveRetonarStatus200_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RestAssured.given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        RestAssured.given()
+                    .basePath("/cozinhas")
+                    .port(port)
+                    .accept(ContentType.JSON)
+                .when()
+                    .get()
+                .then()
+                .body("", Matchers.hasSize(4))
+                .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+    }
+
 }
