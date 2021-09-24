0951c6ca7c1594a5403b4f273b07f8ff33539787
Michel Corrêa <micheloscbc@hotmail.com>
2021-09-24 00:12:21 -0300

    Implementando o endpoint de ativação e inativação de restaurantes

--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -42,6 +42,8 @@ public class Restaurante {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     @CreationTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
     private OffsetDateTime dataCadastro;
@@ -58,4 +60,12 @@ public class Restaurante {
 
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
+
+    public void ativar() {
+        setAtivo(true);
+    }
+
+    public void inativar() {
+        setAtivo(false);
+    }
 }
