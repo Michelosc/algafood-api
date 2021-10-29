31ec2fbba09361af8387cbb9a9d4878937d898b7
Michel Corrêa <micheloscbc@hotmail.com>
2021-10-28 23:07:02 -0300

    Implementando JsonSerializer para customizar representação de paginação

--- src/main/java/com/algaworks/algafoodapi/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/PedidoController.java
@@ -18,6 +18,10 @@ import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -65,10 +69,16 @@ public class PedidoController {
 //    }
 
     @GetMapping
-    public List<PedidoResumoModel> pesquisar( PedidoFilter filtro) {
-        List<Pedido> todosPedidos = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro));
+    public Page<PedidoResumoModel> pesquisar(@PageableDefault(size = 10) PedidoFilter filtro, Pageable pageable) {
+        Page<Pedido> pedidosPage = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro), pageable);
 
-        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+        List<PedidoResumoModel> pedidosResumoModel = pedidoResumoModelAssembler
+                .toCollectionModel(pedidosPage.getContent());
+
+        Page<PedidoResumoModel> pedidosResumoModelPage =
+                new PageImpl<>(pedidosResumoModel,pageable, pedidosPage.getTotalElements());
+
+        return pedidosResumoModelPage;
     }
 
     @GetMapping("/{codigoPedido}")
---
+++ src/main/java/com/algaworks/algafoodapi/core/jackson/PageJsonSerializer.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafoodapi.core.jackson;
+
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.Page;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+
+import java.io.IOException;
+
+@JsonComponent
+public class PageJsonSerializer extends JsonSerializer<Page<?>> {
+
+    @Override
+    public void serialize(Page<?> objects, JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider) throws IOException {
+
+        jsonGenerator.writeStartObject();
+
+        jsonGenerator.writeObjectField("content", objects.getContent());
+        jsonGenerator.writeNumberField("size", objects.getSize());
+        jsonGenerator.writeNumberField("totalElements", objects.getTotalElements());
+        jsonGenerator.writeNumberField("totalPages", objects.getTotalPages());
+        jsonGenerator.writeNumberField("number", objects.getNumber());
+
+        jsonGenerator.writeEndObject();
+    }
+}
