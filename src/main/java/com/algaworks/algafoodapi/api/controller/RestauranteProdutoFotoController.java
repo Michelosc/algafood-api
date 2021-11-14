670f5da1221a26c753c7d2da04aaca9134161033
Michel Correa <micheloscbc@hotmail.com>
2021-11-14 16:35:24 -0300

    Implementando serviÃço de cadastro de foto de produto

--- src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteProdutoFotoController.java
@@ -1,40 +1,53 @@
 package com.algaworks.algafoodapi.api.controller;
 
+import com.algaworks.algafoodapi.api.assembler.FotoProdutoModelAssembler;
+import com.algaworks.algafoodapi.api.model.FotoProdutoModel;
 import com.algaworks.algafoodapi.api.model.input.FotoProdutoInput;
+import com.algaworks.algafoodapi.domain.model.FotoProduto;
+import com.algaworks.algafoodapi.domain.model.Produto;
+import com.algaworks.algafoodapi.domain.service.CadastroProdutoService;
+import com.algaworks.algafoodapi.domain.service.CatalogoFotoProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
 public class RestauranteProdutoFotoController {
 
+    @Autowired
+    private CadastroProdutoService cadastroProdutoService;
+
+    @Autowired
+    private CatalogoFotoProdutoService catalogoFotoProduto;
+
+    @Autowired
+    private FotoProdutoModelAssembler fotoProdutoModelAssembler;
+
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void atualizarFoto(
+    public FotoProdutoModel atualizarFoto(
             @PathVariable Long restauranteId, @PathVariable Long produtoId,
             @Valid FotoProdutoInput fotoProdutoInput) {
 
-        var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
-        var arquivoFoto = Path.of("C:\\Users\\miche\\Downloads\\catalogo", nomeArquivo);
+        Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 
-        System.out.println(fotoProdutoInput.getDescricao());
-        System.out.println(arquivoFoto);
-        System.out.println(fotoProdutoInput.getArquivo().getContentType());
+        MultipartFile arquivo = fotoProdutoInput.getArquivo();
 
-        try {
-            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        FotoProduto foto = new FotoProduto();
+        foto.setProduto(produto);
+        foto.setDescricao(fotoProdutoInput.getDescricao());
+        foto.setContentType(arquivo.getContentType());
+        foto.setTamanho(arquivo.getSize());
+        foto.setNomeArquivo(arquivo.getOriginalFilename());
 
+        FotoProduto fotoSalva = catalogoFotoProduto.salvar(foto);
+
+        return fotoProdutoModelAssembler.toModel(fotoSalva);
     }
 }
---
+++ src/main/java/com/algaworks/algafoodapi/infrastructure/repository/ProdutoRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafoodapi.infrastructure.repository;
+
+import com.algaworks.algafoodapi.domain.model.FotoProduto;
+import com.algaworks.algafoodapi.domain.repository.ProdutoRepositoryQueries;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Repository
+public class ProdutoRepositoryImpl implements ProdutoRepositoryQueries {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Transactional
+    public FotoProduto save(FotoProduto foto) {
+        return manager.merge(foto);
+    }
+}
