0359ab195d0807ea7174139153dfb99c45ad3cd9
Michel Correa <micheloscbc@hotmail.com>
2021-12-04 17:37:24 -0300

    Implementando serviço de envio de e-mail sandbox

--- src/main/java/com/algaworks/algafoodapi/core/email/EmailConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/email/EmailConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algafoodapi.core.email;
 
 import com.algaworks.algafoodapi.domain.service.EnvioEmailService;
 import com.algaworks.algafoodapi.infrastructure.service.email.FakeEnvioEmailService;
+import com.algaworks.algafoodapi.infrastructure.service.email.SandboxEnvioEmailService;
 import com.algaworks.algafoodapi.infrastructure.service.email.SmtpEnvioEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,8 @@ public class EmailConfig {
                 return new FakeEnvioEmailService();
             case SMTP:
                 return new SmtpEnvioEmailService();
+            case SANDBOX:
+                return new SandboxEnvioEmailService();
             default:
                 return null;
         }
