1200c6f9a6d7daf052d54887a47b81fe9be5a558
Michel Corrêa <micheloscbc@hotmail.com>
2021-12-20 22:03:14 -0300

    Entendendo o funcionamento básico de CORS e habilitando na API

--- src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
@@ -20,7 +20,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.http.server.ServletServerHttpRequest;
@@ -53,37 +55,20 @@ public class RestauranteController {
 
     @JsonView(RestauranteView.Resumo.class)
     @GetMapping
-    public List<RestauranteModel> listar() {
-        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
+    public ResponseEntity<List<RestauranteModel>> listar() {
+        List<RestauranteModel> restaurantesModel = restauranteModelAssembler
+                .toCollectionModel(restauranteRepository.findAll());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://127.0.0.1:8000")
+                .body(restaurantesModel);
     }
 
-    @JsonView(RestauranteView.ApenasNome.class)
+    /*@JsonView(RestauranteView.ApenasNome.class)
     @GetMapping(params = "projecao=apenas-nome")
     public List<RestauranteModel> listarApenasNome() {
         return listar();
-    }
-
-//    @GetMapping
-//    public MappingJacksonValue listar(@RequestParam(required = false) String projecao) {
-//        List<RestauranteModel> restauranteModel = restauranteModelAssembler
-//                .toCollectionModel(restauranteRepository.findAll());
-//
-//        MappingJacksonValue restaurantesWrapper = new MappingJacksonValue(restauranteModel);
-//        if("apenas-nome".equals(projecao)) {
-//            restaurantesWrapper.setSerializationView(RestauranteView.ApenasNome.class);
-//        } else if("completo".equals(projecao)) {
-//            restaurantesWrapper.setSerializationView(null);
-//        }
-//
-//        return restaurantesWrapper;
-//    }
-//
-//    @GetMapping
-//    public List<RestauranteModel> listar() {
-//        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
-//    }
-//
-//
+    } */
 
     @Autowired
     private RestauranteModelAssembler restauranteModelAssembler;
