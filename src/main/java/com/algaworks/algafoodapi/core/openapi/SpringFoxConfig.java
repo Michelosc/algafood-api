682d50b5d12cf28d1fdf245b8ea70c8963ec34a1
Michel Corrêa <micheloscbc@hotmail.com>
2022-01-19 22:53:51 -0300

    Adicionando infomações na documentação e adicionando tags e associando com controllers

--- src/main/java/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
@@ -2,7 +2,12 @@ package com.algaworks.algafoodapi.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,7 +18,19 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafoodapi.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .tags(new Tag("Cidades", "Gerencia cidades"));
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Algafood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("1")
+                .contact(new Contact("Michel Corrêa", "https://github.com/Michelosc", "micheloscbc@hotmail.com"))
                 .build();
     }
 }
