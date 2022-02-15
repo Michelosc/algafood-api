e351a19eba2e489e32f57b4980528612f814964b
Michel Corrêa <micheloscbc@hotmail.com>
2022-02-14 23:45:04 -0300

    Descrevendo o modelo de representação de problema

--- src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/Problem.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafoodapi.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.validation.ObjectError;
@@ -9,23 +11,44 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.List;
 
+@ApiModel("Problema")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Builder
 public class Problem {
 
+    @ApiModelProperty(example = "400", position = 1)
     private Integer status;
-    private String type;
-    private String title;
-    private String detail;
-    private String userMessage;
+
+    @ApiModelProperty(example = "2022-02-15T02:33:13.092695374Z", position = 5)
     private OffsetDateTime timestamp;
+
+    @ApiModelProperty(example = "https://algafood.com.br/dados-invalidos", position = 10)
+    private String type;
+
+    @ApiModelProperty(example = "Dados inválidos", position = 15)
+    private String title;
+
+    @ApiModelProperty(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.",
+            position = 20)
+    private String detail;
+
+    @ApiModelProperty(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.",
+        position = 25)
+    private String userMessage;
+
+    @ApiModelProperty(value = "Lista de objetos ou campos que geraram o erro (opcional)", position = 30)
     private List<Object> objects;
 
+    @ApiModel("ObjetoProblema")
     @Getter
     @Builder
     public static class Object {
+
+        @ApiModelProperty(example = "Preço")
         private String name;
+
+        @ApiModelProperty(example = "O preço é obrigatório")
         private String userMessage;
     }
 
