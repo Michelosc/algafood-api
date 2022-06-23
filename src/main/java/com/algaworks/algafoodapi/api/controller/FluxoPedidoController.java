166fff7d2abd4f0553d362a0dec3e8b10213b483
Michel <michel@pop-os.localdomain>
2022-06-22 22:49:51 -0300

    Adicionando links de transições de status de pedidos

--- src/main/java/com/algaworks/algafoodapi/api/controller/FluxoPedidoController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/FluxoPedidoController.java
@@ -16,6 +16,7 @@ import com.algaworks.algafoodapi.domain.service.EmissaoPedidoService;
 import com.algaworks.algafoodapi.domain.service.FluxoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -30,15 +31,18 @@ public class FluxoPedidoController implements FluxoPedidoControllerOpenApi {
 
     @PutMapping("/confirmacao")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void confirmar(@PathVariable String codigoPedido) {
+    public ResponseEntity<Void> confirmar(@PathVariable String codigoPedido) {
         fluxoPedido.confirmar(codigoPedido);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/cancelamento")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void cancelar(@PathVariable String codigoPedido) { fluxoPedido.cancelar(codigoPedido);}
+    public ResponseEntity<Void> cancelar(@PathVariable String codigoPedido) { fluxoPedido.cancelar(codigoPedido);
+        return ResponseEntity.noContent().build();}
 
     @PutMapping("/entrega")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void entregar(@PathVariable String codigoPedido) { fluxoPedido.entregar(codigoPedido);}
+    public ResponseEntity<Void> entregar(@PathVariable String codigoPedido) { fluxoPedido.entregar(codigoPedido);
+        return ResponseEntity.noContent().build();}
 }
