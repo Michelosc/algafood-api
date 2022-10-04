b56eb3d9a47918f559111e78425c207194e09cce
Michel Corrêa <micheloscbc@hotmail.com>
2022-10-04 09:03:57 -0300

    Adicionando links de associação de grupos com permissões.

--- src/main/java/com/algaworks/algafoodapi/api/openapi/controller/GrupoPermissaoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafoodapi/api/openapi/controller/GrupoPermissaoControllerOpenApi.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -20,7 +22,7 @@ public interface GrupoPermissaoControllerOpenApi {
             @ApiResponse(responseCode = "400", description = "ID do grupo inválido", content = @Content(schema = @Schema(implementation = Problem.class))),
             @ApiResponse(responseCode = "404", description = "Grupo não encontrado", content = @Content(schema = @Schema(implementation = Problem.class)))
     })
-    List<PermissaoModel> listar(
+    CollectionModel<PermissaoModel> listar(
             @ApiParam(value = "ID do grupo", example = "1", required = true)
             Long grupoId
     );
@@ -30,7 +32,7 @@ public interface GrupoPermissaoControllerOpenApi {
             @ApiResponse(responseCode = "204", description = "Desassociação realizada com sucesso"),
             @ApiResponse(responseCode = "404", description = "Grupo ou permissão não encontrada", content = @Content(schema = @Schema(implementation = Problem.class)))
     })
-    void desassociar(
+    ResponseEntity<Void> desassociar(
             @ApiParam(value = "ID do grupo", example = "1", required = true)
             Long grupoId,
 
@@ -43,7 +45,7 @@ public interface GrupoPermissaoControllerOpenApi {
             @ApiResponse(responseCode = "204", description = "Associação realizada com sucesso"),
             @ApiResponse(responseCode = "404", description = "Grupo ou permissão não encontrada", content = @Content(schema = @Schema(implementation = Problem.class)))
     })
-    void associar(
+    ResponseEntity<Void> associar(
             @ApiParam(value = "ID do grupo", example = "1", required = true)
                     Long grupoId,
 
