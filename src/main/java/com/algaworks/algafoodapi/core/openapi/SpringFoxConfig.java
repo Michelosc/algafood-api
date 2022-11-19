32afaf2e5893c067193a4d606e170d2355a577aa
Michel Corrêa <micheloscbc@hotmail.com>
2022-11-19 16:11:37 -0300

    Gerando documentação das versões da API com SpringFox e Swagger UI

--- src/main/java/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/openapi/SpringFoxConfig.java
@@ -42,13 +42,14 @@ import java.util.function.Consumer;
 public class SpringFoxConfig {
 
     @Bean
-    public Docket apiDocket() {
+    public Docket apiDocketV1() {
         var typeResolver = new TypeResolver();
 
         return new Docket(DocumentationType.OAS_30)
+                .groupName("V1")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafoodapi.api"))
-                .paths(PathSelectors.any())
+                .paths(PathSelectors.ant("/v1/**"))
                 .build()
                 .useDefaultResponseMessages(false)
                 .globalResponses(HttpMethod.GET, globalGetResponseMessages())
@@ -61,56 +62,31 @@ public class SpringFoxConfig {
                         InputStream.class)
                 .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
                 .directModelSubstitute(Links.class, LinksModelOpenApi.class)
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(PagedModel.class, CozinhaModel.class),
-                        CozinhasModelOpenApi.class
-                ))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(PagedModel.class, PedidoResumoModel.class),
-                        PedidosResumoModelOpenApi.class
-                ))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(CollectionModel.class, ProdutoModel.class),
-                        ProdutosModelOpenApi.class
-                ))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(CollectionModel.class, CidadeModel.class),
-                        CidadesModelOpenApi.class
-                ))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(CollectionModel.class, EstadoModel.class),
-                        EstadosModelOpenApi.class))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(CollectionModel.class, FormaPagamentoModel.class),
-                        FormasPagamentoModelOpenApi.class))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(CollectionModel.class, GrupoModel.class),
-                        GruposModelOpenApi.class
-                ))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(CollectionModel.class, PermissaoModel.class),
-                        PermissoesModelOpenApi.class
-                ))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(CollectionModel.class, RestauranteBasicoModel.class),
-                        RestaurantesBasicoModelOpenApi.class
-                ))
-                .alternateTypeRules(AlternateTypeRules.newRule(
-                        typeResolver.resolve(CollectionModel.class, UsuarioModel.class),
-                        UsuariosModelOpenApi.class
-                ))
-                .apiInfo(apiInfo())
-                .tags(new Tag("Cidades", "Gerencia as cidades"),
-                        new Tag("Grupos", "Gerencia os grupos de usuários"),
-                        new Tag("Cozinhas", "Gerencia as cozinhas"),
-                        new Tag("Formas de pagamento", "Gerencia as formas de pagamento"),
-                        new Tag("Pedidos", "Gerencia os pedidos"),
-                        new Tag("Restaurantes", "Gerencia os restaurantes"),
-                        new Tag("Estados", "Gerencia os estados"),
-                        new Tag("Produtos", "Gerencia os produtos de restaurantes"),
-                        new Tag("Usuários", "Gerencia os usuários"),
-                        new Tag("Estatísticas", "Estatísticas da AlgaFood"),
-                        new Tag("Permissões", "Gerencia as permissões"));
+                .apiInfo(apiInfoV1());
+    }
+
+    @Bean
+    public Docket apiDocketV2() {
+        var typeResolver = new TypeResolver();
+
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("V2")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafoodapi.api"))
+                .paths(PathSelectors.ant("/v2/**"))
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, globalGetResponseMessages())
+                .globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+                .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+                .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+                .additionalModels(typeResolver.resolve(Problem.class))
+                .ignoredParameterTypes(ServletWebRequest.class,
+                        URL.class, URI.class, URLStreamHandler.class, Resource.class, File.class,
+                        InputStream.class)
+                .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+                .directModelSubstitute(Links.class, LinksModelOpenApi.class)
+                .apiInfo(apiInfoV2());
     }
 
     private List<Response> globalPostPutResponseMessages() {
@@ -172,11 +148,20 @@ public class SpringFoxConfig {
         );
     }
 
-    public ApiInfo apiInfo() {
+    public ApiInfo apiInfoV1() {
         return new ApiInfoBuilder()
                 .title("Algafood API")
                 .description("API aberta para clientes e restaurantes")
                 .version("1")
+                .contact(new Contact("Michel Corrêa", "https://github.com/Michelosc", "micheloscbc@hotmail.com"))
+                .build();
+    }
+
+    public ApiInfo apiInfoV2() {
+        return new ApiInfoBuilder()
+                .title("Algafood API")
+                .description("API aberta para clientes e restaurantes")
+                .version("2")
                 .contact(new Contact("Michel Corrêa", "https://github.com/Michelosc", "micheloscbc@hotmail.com"))
                 .build();
     }
