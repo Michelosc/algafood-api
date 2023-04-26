7256ba2954c8569e483b1cb2146b5d84eccfefd0
Michel Correa <michelosc@proton.me>
2023-04-26 09:37:02 -0300

    Configurando o fluxo Authorization Server com Password Credentials e Opaque Tokens

--- src/main/java/com/algaworks/algafoodapi/core/security/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafoodapi/core/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algafoodapi.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,5 +45,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-  
 }
